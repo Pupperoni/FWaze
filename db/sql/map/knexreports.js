10d3be1d8f129e1e3d207ee0a8a0961304b585e4
Ryan Supelana <ryanchester98@gmail.com>
2019-07-19 15:27:27 +0800

    Add fetch methods for reports

--- db/sql/map/knexreports.js
+++ db/sql/map/knexreports.js
@@ -10,10 +10,55 @@ var knex = require('../../knex')
     6 - Minor accident
     7 - Major accident */
 
-function addReport(type, userId){
-    
+function addReport(reportData){
+    return knex.raw('INSERT INTO reports (type, user_id, latitude, longitude) VALUES (?,?,?,?)', [reportData.type, reportData.userId, reportData.latitude, reportData.longitude])
+    .then((row) => {
+        return Promise.resolve(row[0])
+    })
+}
+
+function getAllReports(){
+    return knex.raw('SELECT id, type, votes, latitude, longitude FROM reports')
+    .then((row) => {
+        return Promise.resolve(row[0])
+    })
+}
+
+function getReportsByType(type){
+    return knex.raw('SELECT id, votes, latitude, longitude FROM reports WHERE reports.type = ?', [type])
+    .then((row) => {
+        return Promise.resolve(row[0])
+    })
+}
+
+function getReporterId(reportId){
+    return knex.raw('SELECT users.id FROM reports INNER JOIN users ON reports.user_id = users.id WHERE reports.id = ?', [reportId])
+    .then((row) => {
+        return Promise.resolve(row[0][0])
+    })
+}
+
+function getReportById(id){
+    return knex.raw('SELECT type, votes, latitude, longitude FROM reports WHERE id = ?',[id])
+    .then((row) => {
+        return Promise.resolve(row[0][0])
+    })
+}
+
+function getReportsByBorder(xl, xu, yl, yu){
+
+    return knex.raw('SELECT newpoints.type, users.id, newpoints.latitude, newpoints.longitude FROM (SELECT * FROM reports WHERE latitude > ? and latitude < ? and longitude > ? and longitude < ?) as newpoints INNER JOIN users on newpoints.user_id = users.id', [xl, xu, yl, yu])
+    .then((row) => {ßß
+        return Promise.resolve(row[0])
+    })
 }
 
 module.exports = {
+    addReport: addReport,
+    getAllReports: getAllReports,
+    getReporterId: getReporterId,
+    getReportsByType: getReportsByType,
+    getReportById: getReportById,
+    getReportsByBorder: getReportsByBorder
 
 }
--- routes/map.js
+++ routes/map.js
@@ -4,6 +4,16 @@ const adHandler = require('../db/sql/map/knexadvertisements')
 const reportHandler = require('../db/sql/map/knexreports')
 const userHandler = require('../db/sql/users/knexusers')
 
+const reportTypes = {
+    traffic_jam: 0,
+    heavy_traffic_jam: 1,
+    police: 2,
+    closed_road: 3,
+    car_stopped: 4,
+    construction: 5,
+    minor_accident: 6,
+    major_accident: 7
+}
 // Display map
 router.get('/', (req, res, next) => {
     res.send("Welcome to map | FWaze")
@@ -66,12 +76,50 @@ router.post('/ads/new', (req, res, next) => {
 
 // Get all reports
 router.get('/reports', (req, res, next) => {
-    return res.send("Report list here")
+    reportHandler.getAllReports()
+    .then((results) => {
+        return res.json(results)
+    })
+    .catch((e) => {
+        console.log(e)
+        return res.status(400).json({msg: "Something went wrong. Please try again."})
+    })
 })
 
 // Get report by report id
-router.get('/reports/:id', (req, res, next) => {
-    return res.send(`Report ${req.params.id} here`)
+router.get('/reports/id/:id', (req, res, next) => {
+    reportHandler.getReportById(req.params.id)
+    .then((result) => {
+        return res.json(result)
+    })
+    .catch((e) => {
+        console.log(e)
+        return res.status(400).json({msg: "Something went wrong. Please try again."})
+    })
+})
+
+// Get reports by type
+router.get('/reports/type/:type', (req, res, next) => {
+    reportHandler.getReportsByType(reportTypes[req.params.type])
+    .then((results) => {
+        return res.json(results)
+    })
+    .catch((e) => {
+        console.log(e)
+        return res.status(400).json({msg: "Something went wrong. Please try again."})
+    })
+})
+
+// Get reports by border range
+router.get('/reports/range', (req, res, next) => {
+    reportHandler.getReportsByBorder(50,120,100,150)
+    .then((results) => {
+        return res.json(results)
+    })
+    .catch((e) => {
+        console.log(e)
+        return res.status(400).json({msg: "Something went wrong. Please try again."})
+    })
 })
 
 //
@@ -81,13 +129,20 @@ router.get('/reports/new', (req, res, next) => {
 
 // Add new report
 router.post('/reports/new', (req, res, next) => {
-    // return res.send('Add new report')
      var newReport = {
         type: req.body.type,
         userId: req.body.user_id,
         latitude: req.body.latitude,
         longitude: req.body.longitude
     }
+
+    reportHandler.addReport(newReport)
+    .then((result) => {
+        return res.json({msg: "Success"})
+    })
+    .catch((e) => {
+        return res.status(400).json({msg: "Something went wrong. Check your info and try again."})
+    })
 })
 
 module.exports = router;
