d9e73fa33041b148c2e1e3bfdef3e043e9c73f8b
Ryan Supelana <ryanchester98@gmail.com>
2019-07-19 13:24:46 +0800

    Add longitude to tables

--- db/sql/create_ads_table.js
+++ db/sql/create_ads_table.js
@@ -1,10 +1,12 @@
 var knex = require('../knex')
 
 // Creates a advertisement table
-knex.schema.createTable('advetisements', (table) => {
+knex.schema.createTable('advertisements', (table) => {
     table.increments('id').primary()
     table.string('caption').notNullable()
     table.integer('user_id').unsigned().references('users.id')
+    table.float('latitude',10,6)
+    table.float('longitude',10,6)
     table.timestamp('created_at').defaultTo(knex.fn.now())
     table.timestamp('updated_at').defaultTo(knex.fn.now())
 })
