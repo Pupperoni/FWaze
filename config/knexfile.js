01bee4d29907c33edc10aea0fd8dd14569b6a90c
Ryan Supelana <ryanchester98@gmail.com>
2019-08-19 14:35:27 +0800

    Add knexfile config

--- config/knexfile.js
+++ config/knexfile.js
@@ -1,15 +1,12 @@
 const conn = {
-    client: 'mysql2',
-    connection: {
-      host : 'localhost',
-      port: 3306,
-      user : 'root',
-      password : 'root',
-      database : 'fwaze_db'
-    }
+  client: "mysql2",
+  connection: {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+  }
 };
 
-module.exports = conn
-
-
-  
+module.exports = conn;
