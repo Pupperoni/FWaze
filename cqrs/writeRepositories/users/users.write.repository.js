dea44a7f8a244c645a0780673dca2b186ac889e9
Ryan Supelana <ryanchester98@gmail.com>
2019-08-20 07:55:51 +0800

    Fix bug on extracting last offset from event store

--- cqrs/writeRepositories/users/users.write.repository.js
+++ cqrs/writeRepositories/users/users.write.repository.js
@@ -3,24 +3,26 @@ const redis = new Redis(process.env.REDIS_URL);
 
 module.exports = {
   saveEvent(event) {
+    // extract aggregate from event name (e.g. USER CREATED -> gets USER)
+    var aggregateName = event.eventName.split(" ")[0];
+    // identifier of aggregate
+    var aggregateID = event.payload.id;
+
     // Get last offset from event store
-    redis.zrange("events", 0, -1, "WITHSCORES").then(result => {
-      var offset;
-      // if empty list, start at 0
-      if (result.length == 0) offset = 0;
-      else offset = parseInt(result[result.length - 1]) + 1; // incr 1 last el
+    redis
+      .zrange(`event:${aggregateName}:${aggregateID}`, 0, -1, "WITHSCORES")
+      .then(result => {
+        var offset;
+        // if empty list, start at 0
+        if (result.length == 0) offset = 0;
+        else offset = parseInt(result[result.length - 1]) + 1; // incr 1 last el
 
-      // extract aggregate from event name (e.g. USER CREATED -> gets USER)
-      var aggregateName = event.eventName.split(" ")[0];
-      // identifier of aggregate
-      var aggregateID = event.payload.id;
-
-      // save to eventstore
-      redis.zadd(
-        `event:${aggregateName}:${aggregateID}`,
-        offset,
-        JSON.stringify(event)
-      );
-    });
+        // save to eventstore
+        redis.zadd(
+          `event:${aggregateName}:${aggregateID}`,
+          offset,
+          JSON.stringify(event)
+        );
+      });
   }
 };
